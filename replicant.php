dc8f2e419d0627d86d26d04edae5578ff7b94d8f
EVOKE <evoke.lektrique@gmail.com>
2021-08-12 01:39:58 +0430

    Fixed minimum required wordpress version

--- replicant.php
+++ replicant.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/evokelektrique/replicant
  * Description:       This plugin replicates posts and content in your wordpress websites
  * Version:           0.1
- * Requires at least: 7.2.5
+ * Requires at least: 5.7.0
  * Author:            EVOKE
  * Author URI:        https://github.com/evokelektrique/
  * License:           GPL 3
