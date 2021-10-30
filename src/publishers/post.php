8db8ce22c3e44a1aa0e0b8a63b00c25cde9a26a0
EVOKE <evoke.lektrique@gmail.com>
2021-10-30 16:43:15 +0330

    Remove debug logs

--- src/publishers/post.php
+++ src/publishers/post.php
@@ -19,8 +19,6 @@ class Post {
       $target_node_url = \Replicant\Helper::generate_url_from_node($target_node);
       $response        = $this->perform($body, $target_node_url, $is_update, $is_delete);
       $response        = json_decode($response, true);
-      // TODO: Debug, REMOVE IT
-      error_log(print_r([$is_update, $target_node_url["full"], $response, $get_target_node], true));
       \Replicant\Log::write(
          sprintf(__("%s", "replicant"), $response["message"]),
          $get_target_node->id,
