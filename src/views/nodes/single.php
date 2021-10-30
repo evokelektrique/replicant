01f977924422bfa4a906af5a4df516db75143814
EVOKE <evoke.lektrique@gmail.com>
2021-10-30 14:43:01 +0330

    Update styles

--- src/views/nodes/single.php
+++ src/views/nodes/single.php
@@ -6,23 +6,38 @@ $item = Functions::get( $id );
 ?>
 
 <div class="wrap">
-   <h2>Information</h2>
-   <ul>
-      <li>ID: <b><?= $item->id ?></b></li>
-      <li>NAME: <b><?= $item->name ?></b></li>
-      <li>HOST NAME: <b><?= $item->host ?></b></li>
-      <li>PORT: <b><?= $item->port ?></b></li>
-   </ul>
+
+   <div class="replicant-box">
+      <h2 class="replicant-heading"><?= __("Information", "replicant") ?></h2>
+      <ul class="replicant-node-information">
+         <li>
+            <span class="node-information-head"><?= __("id", "replicant") ?></span>
+            <span class="node-information-value"><?= $item->id ?></span>
+         </li>
+         <li>
+            <span class="node-information-head"><?= __("name", "replicant") ?></span>
+            <span class="node-information-value"><?= $item->name ?></span>
+         </li>
+         <li>
+            <span class="node-information-head"><?= __("host", "replicant") ?></span>
+            <span class="node-information-value"><?= $item->host ?></span>
+         </li>
+         <li>
+            <span class="node-information-head"><?= __("port", "replicant") ?></span>
+            <span class="node-information-value"><?= $item->port ?></span>
+         </li>
+      </ul>
+   </div>
 
    <?php
    // Fetch all logs related to current node
    $node_logs = Log::get_all($item->id, ARRAY_A);
    ?>
-   <h2>Logs</h2>
-   <div class="replicant-logs-container">
-   <?php if(empty($node_logs)): ?>
-      <p class="replicant-text`-danger">No logs found.</p>
-   <?php else: ?>
+   <div class="replicant-logs-container replicant-box">
+      <h2 class="replicant-heading"><?= __("Logs", "replicant") ?></h2>
+      <?php if(empty($node_logs)): ?>
+      <p class="replicant-text`-danger"><?= __("No logs found.", "replicant") ?></p>
+      <?php else: ?>
       <ul>
       <?php foreach($node_logs as $log): ?>
          <?php
@@ -31,14 +46,14 @@ $item = Functions::get( $id );
          $human_readable_level = Log::human_readable_level($log["level"]);
          ?>
          <li>
-            <span>ID: <?= $log["id"] ?></span>
-            <span><?= $human_readable_level ?></span>
-            <span><?= $log["message"] ?></span>
-            <span><?= $human_readable_time . " " . __("ago", "replicant") ?></span>
+            <span class="log-id">#<?= $log["id"] ?></span>
+            <span class="log-level log-level-<?= $log["level"] ?> font-weight-bold"><?= $human_readable_level ?></span>
+            <span class="log-message"><?= $log["message"] ?></span>
+            <span class="log-date"><?= $human_readable_time . " " . __("ago", "replicant") ?></span>
          </li>
       <?php endforeach; ?>
       </ul>
-   <?php endif; ?>
+      <?php endif; ?>
    </div>
 </div>
 
