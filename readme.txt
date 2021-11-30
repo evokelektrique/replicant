a6cb1ecdd8b22010c5a592346f8b80731b04d72f
EVOKE <evoke.lektrique@gmail.com>
2021-11-30 18:53:41 +0330

    Update new version

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://github.com/evokelektrique/replicant
 Tags: Synchronize, Distribute, Replicate
 Requires at least: 5.2
 Tested up to: 5.8
-Stable tag: 0.7.0
+Stable tag: 0.7.1
 Requires PHP: 7.0
 License: AGPLv3
 License URI: https://www.gnu.org/licenses/agpl-3.0.txt
--- replicant.php
+++ replicant.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Replicant
  * Plugin URI:        https://github.com/evokelektrique/replicant
  * Description:       Synchronize your WordPress websites with ease
- * Version:           0.7.0
+ * Version:           0.7.1
  * Requires at least: 5.2
  * Author:            EVOKE
  * Author URI:        https://github.com/evokelektrique/
